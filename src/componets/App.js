a1b15d4f8b808a2e9e79d4d20524947c5d52ce2d
NikolayLiemiekhov <nicklemster@gmail.com>
2022-05-16 09:24:08 +0300

    Add POST button, add config

--- src/componets/App.js
+++ src/componets/App.js
@@ -1,17 +1,25 @@
 import './App.css';
 import axios from "axios";
+import {URL_API} from "../config";
 
 function App() {
-  const clickHandler = () => {
+  const clickGetHandler = () => {
     console.log('click')
-    axios.get(`http://localhost:5000/api/auth/request`)
+    axios.get(`${URL_API}api/auth/request`)
+      .then(res => console.log('response', res.data))
+      .catch(e => console.log('error', e))
+  }
+  const clickPostHandler = () => {
+    console.log('click')
+    axios.post(`${URL_API}api/auth/request`, {surname: 'Liemiekhov'})
       .then(res => console.log('response', res.data))
       .catch(e => console.log('error', e))
   }
 
   return (
     <div className="App">
-      <button type={"button"} onClick={clickHandler}>Send request</button>
+      <button type={"button"} onClick={clickGetHandler}>GET request</button>
+      <button type={"button"} onClick={clickPostHandler}>POST request</button>
     </div>
   );
 }
