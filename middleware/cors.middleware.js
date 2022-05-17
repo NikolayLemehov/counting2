a96d06181103a2e131fbc676e0b69c259748b76c
NikolayLiemiekhov <nicklemster@gmail.com>
2022-05-17 20:39:26 +0300

    Fix cors

--- middleware/cors.middleware.js
+++ middleware/cors.middleware.js
@@ -1,5 +1,17 @@
 function cors(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
+  const origins = [
+    'http://localhost',
+    'https://counting-server-220515.herokuapp.com',
+    'https://counting-client-220515.herokuapp.com'
+  ];
+
+  for(let i = 0; i < origins.length; i++){
+    const origin = origins[i];
+
+    if(req.headers.origin.indexOf(origin) > -1){
+      res.header('Access-Control-Allow-Origin', req.headers.origin);
+    }
+  }
   res.header("Access-Control-Allow-Credentials", "true")
   res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE")
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
