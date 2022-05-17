11a5e48ef02fc18cf65c261869633c32be1ef422
NikolayLiemiekhov <nicklemster@gmail.com>
2022-05-17 20:24:04 +0300

    Fix cors

--- middleware/cors.middleware.js
+++ middleware/cors.middleware.js
@@ -2,7 +2,7 @@ function cors(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*")
   res.header("Access-Control-Allow-Credentials", "true")
   res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE")
-  res.header("Access-Control-Allow-Headers", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
   next()
 }
 
