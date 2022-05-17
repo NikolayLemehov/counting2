054a749667a34d742b38df18a215daecc50abea2
NikolayLiemiekhov <nicklemster@gmail.com>
2022-05-17 20:56:11 +0300

    Fix cors

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express")
 const path = require("path")
-const corsMiddleware = require("middleware/cors.middleware")
+// const corsMiddleware = require("middleware/cors.middleware")
 
 const PORT = process.env.PORT || 80
 console.log(PORT)
 const app = express()
-app.use(corsMiddleware)
+// app.use(corsMiddleware)
 app.use(express.static(__dirname))
 app.use(express.static(path.resolve(__dirname, 'build')))
 
