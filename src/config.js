867d0c1b67b32dadb74780da442f867014317420
NikolayLiemiekhov <nicklemster@gmail.com>
2022-05-27 17:04:01 +0300

    fix client

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-// export const URL_API = 'https://counting-server-220515.herokuapp.com/'
-export const URL_API = 'http://localhost:5000/'
+export const URL_API = 'https://counting-server-220515.herokuapp.com/'
+// export const URL_API = 'http://localhost:5000/'
