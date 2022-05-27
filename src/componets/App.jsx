7ef7017322ce42c3aefa2eb5d36e1bd29f0456da
NikolayLiemiekhov <nicklemster@gmail.com>
2022-05-27 15:36:06 +0300

    client test deploy

--- src/componets/App.jsx
+++ src/componets/App.jsx
@@ -1,20 +1,36 @@
-import {useEffect} from "react";
-import {useDispatch} from "react-redux";
-import {fetchOperations} from "../asyncAction/fetchOperations";
-import {Container} from "@mui/material";
-import ListTable from "./list/ListTable";
-import MyForm from "./MyForm/MyForm";
+// import {useEffect} from "react";
+// import {useDispatch} from "react-redux";
+// import {fetchOperations} from "../asyncAction/fetchOperations";
+// import {Container} from "@mui/material";
+// import ListTable from "./list/ListTable";
+// import MyForm from "./MyForm/MyForm";
 
 function App() {
-  const dispatch = useDispatch()
-  useEffect(() => dispatch(fetchOperations()), [dispatch])
+  // const dispatch = useDispatch()
+  // useEffect(() => dispatch(fetchOperations()), [dispatch])
 
   return (
-    <Container>
-      <h1>Counting</h1>
-      {/*<MyForm/>*/}
-      {/*<ListTable/>*/}
-    </Container>
+    <div className="App">
+      <header className="App-header">
+        {/*<img src={logo} className="App-logo" alt="logo" />*/}
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+    // <Container>
+    //   <h1>Counting</h1>
+    //   {/*<MyForm/>*/}
+    //   {/*<ListTable/>*/}
+    // </Container>
   );
 }
 
